3a10a62821541afd08e8296cd6fa4559fa47867d
Ahmed Azmy <ahmedazmy20@gmail.com>
2025-09-10 15:32:26 +0300

    Add Validation vee-validate & zod , add custom validation roles ar/en

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,9 +23,9 @@ export default defineNuxtConfig({
     baseURL: "/nuxt-task/",
   },
 
-  // nitro: {
-  //   preset: "github-pages",
-  // },
+  nitro: {
+    preset: "github-pages",
+  },
 
   colorMode: {
     preference: "light",
